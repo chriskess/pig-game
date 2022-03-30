9eaf4184e86664eb23c01d5d46fcab5d35863674
Christof Kesselring <christofkesselring@Christofs-MacBook-Pro-2.local>
2022-03-30 10:05:54 +0200

    feat: some new features

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+//Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0EL = document.getElementById('current--0');
+const current1EL = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+//let randomNumber = Math.floor(Math.random(Number) * 6 + 1);
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+//Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  //1. Random Dice roll
+  const dice = Math.trunc(Math.random(Number) * 6) + 1;
+  console.log(dice);
+  //2. Display the Dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  //3.Check for rolled 1
+  if (dice != 1) {
+    //Add dice to the current score
+    currentScore += +dice;
+    current0EL.textContent = currentScore; //CHANGE LATER!
+  }
+  //if true, switch to next player
+  else {
+  }
+});
