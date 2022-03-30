b8a950c8c85acb733af3d00e4da4d32c9e825c31
Christof Kesselring <christofkesselring@Christofs-MacBook-Pro-2.local>
2022-03-30 18:11:32 +0200

    feat: activate player

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //Selecting elements
+const player0EL = document.querySelector('.player--0');
+const player1EL = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0EL = document.getElementById('current--0');
@@ -17,7 +19,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   //1. Random Dice roll
@@ -30,9 +34,16 @@ btnRoll.addEventListener('click', function () {
   if (dice != 1) {
     //Add dice to the current score
     currentScore += +dice;
-    current0EL.textContent = currentScore; //CHANGE LATER!
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
   }
   //if true, switch to next player
   else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0EL.classList.toggle('player--active');
+    player1EL.classList.toggle('player--active');
   }
 });
